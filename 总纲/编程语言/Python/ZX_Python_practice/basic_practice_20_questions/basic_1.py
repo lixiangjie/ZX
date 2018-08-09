5c57099c3407b974d45292b01468844c2fd7db8c
li_xj <li_xj@tebon.com.cn>
2018-08-09 20:33:05 +0800

    提交编程代码与更新导图

--- 总纲/编程语言/Python/ZX_Python_practice/basic_practice_20_questions/basic_1.py
+++ 总纲/编程语言/Python/ZX_Python_practice/basic_practice_20_questions/basic_1.py
@@ -2,24 +2,37 @@
 """
 -------------------------------------------------
    File Name:   bestBaseballPlayer
-   Description:
+   Description: 输入四个整数从大到小排序
    Author:      tebon
    Date:        2018/8/8
 -------------------------------------------------
-   Change Activity:
-                   2018/8/8:
+   Change Activity: 2018/8/8
 -------------------------------------------------
 """
 __author__ = 'tebon'
 
+# 获取输入数值
+def getInputDatas():
+    inputDatas = list(iter(input, 'exit'))  # 输入多个数字存入list
+    inputDatas_int = []
+    # index = 0
+    for data in inputDatas:
+        inputDatas_int.append(int(data))
+        # inputDatas_int.insert(++index, int(data))
+    return inputDatas_int
 
+# 数值排序
+def datasSort(data_list):
+    data_list.sort()
+    return data_list
 
+# 打印输出结果
+def printSortResult(sortedDatasList):
+    print(sortedDatasList)
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    inputDatas_list = getInputDatas()
+    sortedDatasList = datasSort(inputDatas_list)
+    printSortResult(sortedDatasList)
 
 
