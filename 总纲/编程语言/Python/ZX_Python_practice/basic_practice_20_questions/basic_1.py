90331140798bb15eed2901d7befbf5c62f5b7533
li_xj <li_xj@tebon.com.cn>
2018-08-13 11:37:29 +0800

    重急ppt

--- 总纲/编程语言/Python/ZX_Python_practice/basic_practice_20_questions/basic_1.py
+++ 总纲/编程语言/Python/ZX_Python_practice/basic_practice_20_questions/basic_1.py
@@ -13,13 +13,9 @@ __author__ = 'tebon'
 
 # 获取输入数值
 def getInputDatas():
-    inputDatas = list(iter(input, 'exit'))  # 输入多个数字存入list
-    inputDatas_int = []
-    # index = 0
-    for data in inputDatas:
-        inputDatas_int.append(int(data))
-        # inputDatas_int.insert(++index, int(data))
-    return inputDatas_int
+    inputDatas_list = list(iter(input, 'exit'))  # 输入多个数字存入list
+    inputDatas = [int(data) for data in inputDatas_list]
+    return inputDatas
 
 # 数值排序
 def datasSort(data_list):
@@ -31,8 +27,28 @@ def printSortResult(sortedDatasList):
     print(sortedDatasList)
 
 if __name__ == '__main__':
-    inputDatas_list = getInputDatas()
+    inputDatas = getInputDatas()
     sortedDatasList = datasSort(inputDatas_list)
     printSortResult(sortedDatasList)
+
+
+"""
+Q1：list['4','3','2','1'] 数字型字符串转数字
+A1：方法改进过程：
+    1.  index = 0
+        result = []
+        for data in list:
+            result.insert(++index, int(data))
+        return result
+        
+    2.  result = []
+        for data in list:
+            result.append(int(data))
+        return result
+        
+    3.  result = [int(data) for data in list]
+        return result
+    
+"""
 
 
